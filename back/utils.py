2ccffaedd6d6d9edaf4634fb00658e1ef54c114c
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-01-30 17:53:47 +0100

    Sécurisation de la secret Key

--- back/utils.py
+++ back/utils.py
@@ -1,9 +1,9 @@
 import jwt
 from datetime import datetime, timezone, timedelta
-SECRET_KEY = "secret"
+from django.conf import settings  # Importation de settings
 
 def generate_jwt():
     """Génère un JWT simple"""
     expiration_time = datetime.now(tz=timezone.utc) + timedelta(minutes=30)
-    encoded = jwt.encode({"some": "payload", "exp": expiration_time}, SECRET_KEY, algorithm="HS256")
-    return {"access_tokken" : encoded}
+    encoded = jwt.encode({"some": "payload", "exp": expiration_time}, settings.SECRET_KEY, algorithm="HS256")
+    return {"access_token": encoded}
--- planningBack/settings.py
+++ planningBack/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%xnosre*z^@4y)s_1n)d3w*qb12!fljp+$by8jbf@d@6ps#m%&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
