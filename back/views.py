0e340750c1b7822c2d531eb873028045a120150f
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-08-05 10:14:19 +0200

    Mise a jour

--- back/views.py
+++ back/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie, csrf_exempt
 from rest_framework.response import Response
 from django.http import JsonResponse
 from rest_framework.permissions import AllowAny, IsAuthenticated
